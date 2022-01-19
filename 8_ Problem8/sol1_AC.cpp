7fb2073d1ee49456758c9549db002ef4bb80995f
alabh <alabenhmouda@gmail.com>
2022-01-19 19:46:07 +0100

    add solution to problem 8

---
+++ 8_ Problem8/sol1_AC.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const char nl = '\n';
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int a;
+        cin >> a;
+        cout << a << nl;
+    }
+    return 0;
+}
