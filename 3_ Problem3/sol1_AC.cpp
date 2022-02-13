3c9b0b7f496beb3b52e9af81cceb99f8d82b5f6f
Ahmed Mahfoudhi <53706063+Ahmed-Mahfoudhi@users.noreply.github.com>
2022-02-13 14:48:09 +0100

    Update sol1_AC.cpp

---
+++ 3_ Problem3/sol1_AC.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+void solve(){
+    int n;
+    cin >> n;
+    string a,b;
+    cin >> a >> b;
+
+    for(int i = 1; i<n; ++i){
+        if(a[i] == '1' && b[i] == '1'){
+            cout << "NO\n";
+            return;
+        }
+    }
+    cout << "YES\n";
+}
+ 
+ 
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int tests = 1;
+    cin >> tests;
+    while(tests--){
+        solve();
+    }
+}
