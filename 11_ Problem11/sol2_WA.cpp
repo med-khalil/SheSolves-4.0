c4e507a0740d77bbb4588fdc3fe1b6b6eb4ad5cd
Lquit <56963723+Lquit@users.noreply.github.com>
2022-02-26 18:33:48 +0100

    Update and rename sol2_TLE.cpp to sol2_WA.cpp

---
+++ 11_ Problem11/sol2_WA.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std ;
+
+int main()
+{
+    int t , MOD = 1e9+7 ;
+    cin >> t ;
+    while(t--)
+    {
+        int n ;
+        cin >> n ;
+        int prev = 1 , temp , ans = 1 ;
+        if(n==1)
+        {
+            cout << 0 << endl ;
+        }
+        else
+        {
+            n -= 2 ;
+            while(n--)
+            {
+                temp = ans ;
+                ans = (prev + ans) % MOD ;
+                prev = temp ;
+            }
+            cout << ans << endl ;
+        }
+    }
+}
